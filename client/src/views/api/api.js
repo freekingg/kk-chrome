d8fe368d3ac01419fa4a0da45b049c617da22ac1
king <xianxin001@gmail.com>
2023-06-18 15:10:59 +0400

    优化app

--- client/src/views/api/api.js
+++ client/src/views/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-let baseURL = location.origin + '/'
-// let baseURL = 'http://localhost:52939' + '/'
+// let baseURL = location.origin + '/'
+let baseURL = process.env.NODE_ENV === "development" ? 'http://localhost:52939' + '/' : location.origin + '/'
+console.log('baseURL: ', baseURL);
 const request = (obj) => {
   return new Promise((resolve, reject) => {
     axios({
